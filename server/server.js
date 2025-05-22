3bf01be9ad164fe9149ccee5aa56275003b43d13
Polly Randle <randlepolly@gmail.com>
2025-05-22 12:59:11 +0100

    mongoose model for users - api checked postman

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// TODO: Add your MongoDB connection string
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,3 +22,8 @@ app.get('/', (req, res) => res.send('API Running'));
 // Start server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// users
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
